530092227fc236559c2271848680dedca6f756cc
zephyrus <luo@LuoMacBook-Air.local>
2025-05-27 17:42:25 +0800

    修复Windows构建错误: 将CreateAndShow替换为Create和Show两个分离的方法调用

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,9 +27,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 800);
-  if (!window.CreateAndShow(L"物资验收反作弊工具", origin, size)) {
+  if (!window.Create(L"物资验收反作弊工具", origin, size)) {
     return EXIT_FAILURE;
   }
+  window.Show();
   window.SetQuitOnClose(true);
 
   ::MSG msg;
